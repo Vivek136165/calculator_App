4a35daa15bc337480af4439b0514b041d7192500
91620 <2201020009@cgu-odisha.ac.in>
2023-04-05 15:15:15 +0530

    Calculator app -commit

--- app/src/main/java/com/example/calculator_app/MainActivity.java
+++ app/src/main/java/com/example/calculator_app/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.calculator_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText num1;
+    private EditText num2;
+    private RadioButton sum;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1=Edittext
     }
 }
